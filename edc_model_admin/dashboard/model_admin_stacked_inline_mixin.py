ddef36b21486bb62203e421f8db863b2dfa6576b
erikvw <ew2789@gmail.com>
2024-01-26 09:10:40 -0600

    move post-migrate, autodiscover and/or system checks to edc-appconfig

--- edc_model_admin/dashboard/model_admin_stacked_inline_mixin.py
+++ edc_model_admin/dashboard/model_admin_stacked_inline_mixin.py
@@ -1,6 +1,6 @@
 from django_audit_fields.admin import ModelAdminAuditFieldsMixin
 from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
-from edc_notification import NotificationModelAdminMixin
+from edc_notification.modeladmin_mixins import NotificationModelAdminMixin
 
 from edc_model_admin.mixins import (
     ModelAdminFormAutoNumberMixin,
--- runtests.py
+++ runtests.py
@@ -2,7 +2,6 @@
 import logging
 from pathlib import Path
 
-from edc_constants.constants import IGNORE
 from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_model_admin"
@@ -13,6 +12,7 @@ project_settings = DefaultTestSettings(
     template_dirs=[str(base_dir / app_name / "tests" / "templates")],
     BASE_DIR=base_dir,
     APP_NAME=app_name,
+    SILENCED_SYSTEM_CHECKS=["sites.E101", "edc_navbar.E002", "edc_navbar.E003"],
     ROOT_URLCONF="model_admin_app.urls",
     ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
     SUBJECT_SCREENING_MODEL="model_admin_app.subjectscreening",
@@ -21,7 +21,6 @@ project_settings = DefaultTestSettings(
     SUBJECT_VISIT_MISSED_MODEL="edc_visit_tracking.subjectvisitmissed",
     SUBJECT_REQUISITION_MODEL="model_admin_app.subjectrequisition",
     SUBJECT_APP_LABEL="model_admin_app",
-    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
     EDC_SITES_REGISTER_DEFAULT=True,
     INSTALLED_APPS=[
         "django.contrib.admin",
@@ -34,29 +33,28 @@ project_settings = DefaultTestSettings(
         "django_extensions",
         "django_crypto_fields.apps.AppConfig",
         "django_revision.apps.AppConfig",
-        "edc_auth.apps.AppConfig",
-        "edc_adverse_event.apps.AppConfig",
         "edc_action_item.apps.AppConfig",
+        "edc_adverse_event.apps.AppConfig",
         "edc_appointment.apps.AppConfig",
-        "edc_consent.apps.AppConfig",
-        "edc_crf.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
         "edc_dashboard.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
         "edc_device.apps.AppConfig",
         "edc_export.apps.AppConfig",
-        "edc_identifier.apps.AppConfig",
         "edc_facility.apps.AppConfig",
-        "edc_metadata.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
         "edc_lab.apps.AppConfig",
-        "edc_list_data.apps.AppConfig",
+        "edc_label.apps.AppConfig",
+        "edc_locator.apps.AppConfig",
         "edc_listboard.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
         "edc_notification.apps.AppConfig",
         "edc_offstudy.apps.AppConfig",
-        "edc_protocol.apps.AppConfig",
-        "edc_randomization.apps.AppConfig",
         "edc_registration.apps.AppConfig",
         "edc_review_dashboard.apps.AppConfig",
-        "edc_sites.apps.AppConfig",
         "edc_subject_dashboard.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
         "edc_timepoint.apps.AppConfig",
         "edc_visit_schedule.apps.AppConfig",
         "edc_visit_tracking.apps.AppConfig",
@@ -64,6 +62,7 @@ project_settings = DefaultTestSettings(
         "edc_navbar.apps.AppConfig",
         "adverse_event_app",
         "model_admin_app.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
     ],
     DASHBOARD_BASE_TEMPLATES={
         "dashboard_template": str(

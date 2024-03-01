7ba2cd72448b0d4563a5f1510f2c9335b879e3ec
erikvw <ew2789@gmail.com>
2024-02-29 22:45:34 -0600

    use RequiresConsentModelAdminMixin

--- edc_model_admin/dashboard/model_admin_crf_dashboard_mixin.py
+++ edc_model_admin/dashboard/model_admin_crf_dashboard_mixin.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from django.core.exceptions import ObjectDoesNotExist
+from edc_consent.modeladmin_mixins import RequiresConsentModelAdminMixin
 from edc_fieldsets import FieldsetsModelAdminMixin
 from edc_visit_tracking.modeladmin_mixins import CrfModelAdminMixin
 
@@ -9,6 +10,7 @@ from .model_admin_subject_dashboard_mixin import ModelAdminSubjectDashboardMixin
 
 class ModelAdminCrfDashboardMixin(
     FieldsetsModelAdminMixin,
+    RequiresConsentModelAdminMixin,
     ModelAdminSubjectDashboardMixin,
     CrfModelAdminMixin,
 ):

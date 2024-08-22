97053a0afe7e314f9c89f515f285bf234bd4eef4
erikvw <ew2789@gmail.com>
2024-08-22 16:36:42 -0500

    add method to safely render change list note

--- edc_model_admin/mixins/templates_model_admin_mixin.py
+++ edc_model_admin/mixins/templates_model_admin_mixin.py
@@ -34,6 +34,16 @@ class TemplatesModelAdminMixin:
     delete_confirmation_template: str = "edc_model_admin/admin/delete_confirmation.html"
     change_form_title: str | None = None
 
+    def rendered_change_list_note(self):
+        """Override if the class attr `change_list_note` is more than
+        a normal string.
+
+        For example, if the rendering attempts to resolve a URL for
+        a class not yet registered, modeladmin registration silently
+        stops registering the remaining modeladmin classes.
+        """
+        return self.change_list_note
+
     def change_view(self, request, object_id, form_url="", extra_context=None):
         extra_context = extra_context or {}
         extra_context.update(
@@ -53,7 +63,7 @@ class TemplatesModelAdminMixin:
         extra_context.update(
             {
                 "show_object_tools": self.show_object_tools,
-                "change_list_note": self.change_list_note,
+                "change_list_note": self.rendered_change_list_note,
                 "change_list_help": self.change_list_help,
             }
         )
